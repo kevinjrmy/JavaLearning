e4ac9b114f0a6fd25cc0d0c31c4182cd51629bc1
Kevin Jeremy <kevinjrmy@gmail.com>
2020-08-28 13:41:20 +0800

    working with 2D arrays

--- src/NewPlayground.java
+++ src/NewPlayground.java
@@ -1,22 +1,16 @@
-import java.util.List;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Scanner;
-
-
 public class NewPlayground {
 
 	public static void main(String[] args) {
-		LinkedList<String> names = new LinkedList<String>();
-		names.push("Kevin");
-		names.push("Janiel");
-		names.push("Mari");
-		
-		System.out.println(names.remove());
-		System.out.println(names.remove());
-		System.out.println(names.remove());
+		int[][] data = {
+				{4, 6, 3, 10},
+				{4, 2, 40, 1},
+				{5, 34, 1, 43},
+		};
+		for(int i = 0; i < data.length; i++){
+			for(int j = 0; j < data[i].length; j++){
+				System.out.print(data[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
-
 }
